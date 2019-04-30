89ca89659a16ba2981d324270a1cb297d3cac389
Jaideep Jha (INFOSEC) <jjaideep@microsoft.com>
2019-04-30 14:06:39 +0530

    Added angular project, with simple form in the home page

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {QuestionComponent} from './question.component'
+
+@NgModule({
+  declarations: [
+    AppComponent, QuestionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/src/app/question.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: "question",
+    templateUrl: "question.component.html"
+})
+
+export class QuestionComponent{
+
+}
